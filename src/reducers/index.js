507f8cca3da72ea5269c736f2948948135bc1938
Mihai Macaneata <themacaneata@gmail.com>
2019-11-18 16:43:33 +0200

    fix export reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,17 +3,4 @@
  * @module reducers/root
  */
 
-import chart_data_visualization from './chart_data_visualization';
-
-/**
- * Root reducer.
- * @function
- * @param {Object} state Current state.
- * @param {Object} action Action to be handled.
- * @returns {Object} New state.
- */
-const reducers = {
-  chart_data_visualization
-};
-
-export default reducers;
+export { chart_data_visualization } from './chart_data_visualization';
