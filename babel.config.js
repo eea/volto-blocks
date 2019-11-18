b09aa22cd19d6d1b3d085e32e2b578e435b3db7b
Mihai Macaneata <themacaneata@gmail.com>
2019-11-18 17:32:54 +0200

    remove babel plugins

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = function(api) {
     api.cache(true);
   
     const presets = ['@babel/preset-env', '@babel/preset-react'];
-    const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining', "transform-runtime", "syntax-dynamic-import", "react-loadable/babel"];
+    // const plugins = [require('@babel/plugin-proposal-class-properties'), '@babel/plugin-proposal-optional-chaining', "transform-runtime", "syntax-dynamic-import", "react-loadable/babel"];
+    const plugins []
     return {
       presets,
       plugins
