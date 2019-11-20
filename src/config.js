04681590d393821d9b20008ee90cd5f61a629a92
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-21 00:08:16 +0200

    Moved stuff to other packages

--- src/config.js
+++ src/config.js
@@ -2,17 +2,8 @@
 import ChartBlockEdit from './ChartBlock/ChartBlockEdit';
 import ChartBlockView from './ChartBlock/ChartBlockView';
 
-import EuropeCompareBlockEdit from './EuropeCompareBlock/Edit';
-import EuropeCompareBlockView from './EuropeCompareBlock/View';
-
-import EuropeForestBlockEdit from './EuropeForestBlock/Edit';
-import EuropeForestBlockView from './EuropeForestBlock/View';
-
-import PlotlyBlockEdit from './PlotlyChart/Edit';
-import PlotlyBlockView from './PlotlyChart/View';
-
-import TableauBlockEdit from './TableauBlock/TableauBlockEdit';
-import tableauBlockView from './TableauBlock/TableauBlockView';
+// import TableauBlockEdit from './TableauBlock/TableauBlockEdit';
+// import tableauBlockView from './TableauBlock/TableauBlockView';
 
 import TextBlockEdit from './Ckeditor/Edit';
 import TextBlockView from './Ckeditor/View';
