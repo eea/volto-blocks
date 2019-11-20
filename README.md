1cdc9f72c02ef78d0e9b424e1e6321bcfdd85a52
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-20 21:11:30 +0200

    improve package

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # volto-blocks
+
+Generic Volto blocks: new text editors, charts, etc.
---
+++ src/config.js
@@ -0,0 +1,21 @@
+
+import ChartBlockEdit from './ChartBlock/ChartBlockEdit';
+import ChartBlockView from './ChartBlock/ChartBlockView';
+
+import EuropeCompareBlockEdit from './EuropeCompareBlock/Edit';
+import EuropeCompareBlockView from './EuropeCompareBlock/View';
+
+import EuropeForestBlockEdit from './EuropeForestBlock/Edit';
+import EuropeForestBlockView from './EuropeForestBlock/View';
+
+import PlotlyBlockEdit from './PlotlyChart/Edit';
+import PlotlyBlockView from './PlotlyChart/View';
+
+import TableauBlockEdit from './TableauBlock/TableauBlockEdit';
+import tableauBlockView from './TableauBlock/TableauBlockView';
+
+import TextBlockEdit from './Ckeditor/Edit';
+import TextBlockView from './Ckeditor/View';
+
+import TextBlockEditWysiwyg from './Text/Edit';
+import TextBlockViewWysiwyg from './Text/View';
