0fee53394497956f0a0f02c9dc3e7fb2d1fb6051
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-24 11:18:32 +0200

    Added addCustomGroup function

--- src/config.js
+++ src/config.js
@@ -1,7 +1,21 @@
 import ChildrenListView from './ChildrenList/View';
 import ChildrenListEdit from './ChildrenList/Edit';
 
+function addCustomGroup(config) {
+  const hasCustomGroup = config.blocks.groupBlocksOrder.filter(
+    el => el.id === 'custom_addons',
+  );
+  if (!hasCustomGroup.length) {
+    config.blocks.groupBlocksOrder.push({
+      id: 'custom_addons',
+      title: 'Custom addons',
+    });
+  }
+}
+
 export function applyConfig(config) {
+  addCustomGroup(config);
+
   config.blocks.blocksConfig.childrenlist = {
     id: 'childrenlist',
     group: 'custom_addons',
